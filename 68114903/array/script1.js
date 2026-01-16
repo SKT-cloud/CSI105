dbc7d08291bbb4aeb7f8c3fcdc4191e63060c3a6
TonySK <suwijuck.kat@spumail.net>
2026-01-16 11:44:57 +0700

    update

--- 68114903/array/script1.js
+++ 68114903/array/script1.js
@@ -8,10 +8,18 @@ function addData() {
     let num = parseFloat(n.value);
     arr.unshift(num);
     out.innerHTML = "";
+    n.value = "";
     for (let i = 0; i < arr.length; i++) {
         out.innerHTML = out.innerHTML+arr[i] + "<br>";
     }
     console.log(arr);
-    // console.log("add");
+}
+
+function deleteData() {
+    arr.shift();
+    out.innerHTML = "";
+    for (let i = 0; i < arr.length; i++) {
+        out.innerHTML = out.innerHTML+arr[i] + "<br>";
+    }
 }
 
