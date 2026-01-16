5f0c6ef03bc30fe59880f1a93c9aed5abafc4a07
SKT-cloud <suwijuck.kat@spumail.net>
2026-01-16 10:01:17 +0700

    Add files via upload

---
+++ 68114903/array/script.js
@@ -0,0 +1,12 @@
+let score = [85,50,96,40,35]
+console.log("score = ",score);
+
+let car = ["Toyota", "Honda", "Ford", "Chevrolet"]
+console.log("car = ", car);
+
+let score1 = []
+console.log(score1);
+console.log(score[2]);
+console.log(car[0]);
+score[3] = 45
+console.log("score after update = ", score);
