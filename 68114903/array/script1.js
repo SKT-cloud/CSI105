2fa7841a20b42862bad203d245692cfc43702331
TonySK <suwijuck.kat@spumail.net>
2026-01-16 11:25:19 +0700

    Update

---
+++ 68114903/array/script1.js
@@ -0,0 +1,11 @@
+const n = document.getElementById("num");
+console.log(n);
+const out = document.getElementById("result");
+console.log(out);
+let arr = [];
+
+function addData() {
+    let num = parseFloat(n.value);
+    console.log(typeof(num));
+    // console.log("add");
+}
