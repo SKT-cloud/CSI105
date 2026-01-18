95882fdd8415ca52d8feb4166e524f0049aba809
SKT-cloud <suwijuck.kat@spumail.net>
2026-01-19 01:08:01 +0700

    homework done!!
    add reset
    add multiply

--- 68114903/array/script1.js
+++ 68114903/array/script1.js
@@ -2,6 +2,8 @@ const n = document.getElementById("num");
 console.log(n);
 const out = document.getElementById("result");
 console.log(out);
+const multiply = document.getElementById("multiply");
+console.log(multiply);
 let arr = [];
 
 function addData() {
@@ -10,16 +12,51 @@ function addData() {
     out.innerHTML = "";
     n.value = "";
     for (let i = 0; i < arr.length; i++) {
-        out.innerHTML = out.innerHTML+arr[i] + "<br>";
+        out.innerHTML += arr[i] + "<br>";
     }
     console.log(arr);
+    calculateMultiply();
 }
 
 function deleteData() {
     arr.shift();
     out.innerHTML = "";
+    n.value = "";
     for (let i = 0; i < arr.length; i++) {
-        out.innerHTML = out.innerHTML+arr[i] + "<br>";
+        out.innerHTML += arr[i] + "<br>";
+    }
+    calculateMultiply();
+}
+
+function calculateMultiply() {
+    multiply.innerHTML = "";
+
+    let even = 1;
+    let odd = 1;
+    let hasEven = false;
+    let hasOdd = false;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            even *= arr[i];
+            hasEven = true;
+        } else {
+            odd *= arr[i];
+            hasOdd = true;
+        }
+    }
+
+    if (hasEven) {
+        multiply.innerHTML += "ผลคูณเลขคู่: " + even + "<br>";
+    }
+
+    if (hasOdd) {
+        multiply.innerHTML += "ผลคูณเลขคี่: " + odd;
     }
 }
 
+function reset() {
+    arr = [];
+    out.innerHTML = "";
+    multiply.innerHTML = "";
+}
