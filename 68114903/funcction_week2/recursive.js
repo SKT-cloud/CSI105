1b0c2ec48616a8386491c24fb1be85fee07e6166
TonySK <suwijuck.kat@spumail.net>
2026-01-23 10:34:12 +0700

    Update recursive.js

--- 68114903/funcction_week2/recursive.js
+++ 68114903/funcction_week2/recursive.js
@@ -4,3 +4,12 @@ for(let i = 5; i>0; i--){
     mul *= i
     console.log("mul = ", mul);
 }
+
+function factorial(n){
+    if (n==1 || n==0){
+        return 1
+    }else{
+        return n*factorial(n-1)
+    }
+}
+console.log("This function = ",factorial(5));
