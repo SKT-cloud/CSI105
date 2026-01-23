41700a259c4e0e8ad3cdb1401fc534b9fc3958e2
TonySK <suwijuck.kat@spumail.net>
2026-01-23 10:47:04 +0700

    Add hw.html, hw.js, and update recursive.js
    Added new HTML and JS files for homework setup. Updated recursive.js with improved comments, formatting, and added a new recursive function functionX that calculates 2 to the power of n.

--- 68114903/funcction_week2/recursive.js
+++ 68114903/funcction_week2/recursive.js
@@ -1,15 +1,24 @@
-// recursive คือ fubction ที่เรียกตัวเองวนไปเรื่อยๆ
-let mul = 1
-for(let i = 5; i>0; i--){
-    mul *= i
-    console.log("mul = ", mul);
+// recursive คือ fubction ที่เรียกตัวเองวนไปเรื่อยๆ จนกว่าจะถึง base case ที่กำหนดไว้
+let mul = 1;
+for (let i = 5; i > 0; i--) {
+  mul *= i;
+  console.log("mul = ", mul);
 }
 
-function factorial(n){
-    if (n==1 || n==0){
-        return 1
-    }else{
-        return n*factorial(n-1)
-    }
+function factorial(n) {
+  if (n == 1 || n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-console.log("This function = ",factorial(5));
+console.log("This function = ", factorial(5));
+
+function functionX(n) {
+  if (n == 0) {
+    return (n = 1);
+  } else {
+    return functionX(n - 1) * 2;
+  }
+}
+console.log(functionX(5));
