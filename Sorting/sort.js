774e95e54f2ca57e1e27c6b7307c89f73f6587b8
TonySK <suwijuck.kat@spumail.net>
2026-03-20 10:05:08 +0700

    Update sort.js

--- Sorting/sort.js
+++ Sorting/sort.js
@@ -1,5 +1,15 @@
 class sort{
     insertionSort(arr){
-        
+        let key,j
+        for(let i=1; i<arr.length; i++){
+            key = arr[i]
+            j = i-1
+            while(j>=0 && key<arr[j]){
+                arr[j+1] = arr[j]
+                j=j-1
+            }
+            arr[j+1] = key
+        }
     }
 }
+
